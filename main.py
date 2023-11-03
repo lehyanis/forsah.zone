1c9b47dbc0098a15d4f556daafe79564853743f6
lehyanis <34630335+lehyanis@users.noreply.github.com>
2023-11-03 13:05:27 -0700

    Update main.py

--- main.py
+++ main.py
@@ -11,41 +11,16 @@ db = SQLAlchemy(app)
 @app.route('/')
 def index():
     '''Returns the homepage'''
-    with db.engine.connect() as conn:
-        results = conn.execute("SELECT * FROM avg_size").fetchall()
-
-        trade_data = conn.execute(
-            '''SELECT symbol,date,DATE_FORMAT(time, '%H:%i:%s') AS time, close,price,size,conditions,Quote,`Signal`,QuoteCond, EMA
-                FROM trades 
-                where time between '16:00' and '23:00' 
-                and date BETWEEN DATE_SUB((SELECT MAX(date) FROM trades), INTERVAL 3 DAY) AND (SELECT MAX(date) FROM trades)
-                and conditions like '%Qualified%' or '%Sweep%' or '%Average Price%' or '%Derivatively%' or '%Out of Sequence%' 
-                '''
-            ).fetchall()
+    data1 = db.session.execute(text("SELECT * FROM SelectedTrades")).fetchall()
+    data2 = db.session.execute(text("SELECT * FROM VolumeDeltaSummary")).fetchall()
         
-        spy_only = conn.execute(
-            '''SELECT symbol, date, time, close, price, size, conditions, Quote, QuoteCond, `Signal`, EMA FROM trades
-                where size >= 200000 and symbol = 'SPY' 
-                and date BETWEEN DATE_SUB((SELECT MAX(date) FROM trades), INTERVAL 5 DAY) AND (SELECT MAX(date) FROM trades)
-                and conditions like '%Qualified%'
-                order by size DESC; 
-                '''
-            ).fetchall()
-        
-    return render_template('index.html', summary=results, trades=trade_data, spy=spy_only)
+    return render_template('index.html', summary=data2, trades=data1)
 
 
 @app.route('/home')
 def home():
     '''Returns the homepage'''
     return render_template('home.html')
-
-@app.route('/summary')
-def summary():
-    '''Returns a list of trades from the database'''
-    with db.engine.connect() as conn:
-        results = conn.execute("SELECT * FROM avg_size").fetchall()
-    return render_template('summary.html', summary=results)
 
 
 if __name__ == '__main__':
