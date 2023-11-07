9b799de954c2b9480ffd519b0e827f8190dcf653
lehyanis <34630335+lehyanis@users.noreply.github.com>
2023-11-07 08:49:50 -0800

    Update main.py

--- main.py
+++ main.py
@@ -4,34 +4,47 @@ import pymysql.cursors
 import os
 
 app = Flask(__name__)
-connection = pymysql.connect(host='monorail.proxy.rlwy.net',
-                                port=55068,
-                                user='root',
-                                password='46f23AA--1Ga3h62GfcFGGF35GBcBEa2',
-                                database='railway',
-                                charset='utf8mb4',
-                                cursorclass=pymysql.cursors.DictCursor,
-                                client_flag=pymysql.constants.CLIENT.MULTI_STATEMENTS)
+
+def get_db_connection():
+    if 'db_connection' not in g:
+        g.db_connection = pymysql.connect(
+            host='monorail.proxy.rlwy.net',
+            port=55068,
+            user='root',
+            password='46f23AA--1Ga3h62GfcFGGF35GBcBEa2',
+            database='railway',
+            charset='utf8mb4',
+            cursorclass=pymysql.cursors.DictCursor,
+            client_flag=pymysql.constants.CLIENT.MULTI_STATEMENTS
+        )
+    return g.db_connection
+
+@app.before_request
+def before_request():
+    g.db = get_db_connection()
+
+@app.teardown_request
+def teardown_request(exception=None):
+    db_connection = g.pop('db_connection', None)
+
+    if db_connection is not None:
+        db_connection.close()
 
 @app.route('/')
 def index():
-    with connection.cursor() as cursor:
-        sql = "SELECT * FROM SelectedTrades"
-        cursor.execute(sql)
-        SelectedTrades = cursor.fetchall()
-        
-        sql2 = "SELECT * FROM VolumeDeltaSummary"
-        cursor.execute(sql2)
-        VolumeDeltaSummary = cursor.fetchall()
-   
+    '''Returns the homepage'''
+    cursor = g.db.cursor()
+    sql = "SELECT * FROM SelectedTrades"
+    cursor.execute(sql)
+    SelectedTrades = cursor.fetchall()
+    
+    sql2 = "SELECT * FROM VolumeDeltaSummary"
+    cursor.execute(sql2)
+    VolumeDeltaSummary = cursor.fetchall()
+
     return render_template('index.html', summary=VolumeDeltaSummary, trades=SelectedTrades)
 
-
-@app.route('/home')
-def home():
-    '''Returns the homepage'''
-    return render_template('home.html')
-
+# ... rest of the app
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
